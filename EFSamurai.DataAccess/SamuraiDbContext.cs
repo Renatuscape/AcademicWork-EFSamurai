89f4dd54f61c9da5b6e2f384ff5e516d4092419c
Renatuscape <silarcta@gmail.com>
2023-12-01 14:45:27 +0100

    Created DbContext file

---
+++ EFSamurai.DataAccess/SamuraiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFSamurai.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFSamurai.DataAccess
+{
+    public class SamuraiDbContext : DbContext
+    {
+        public DbSet<Samurai> Samurai => Set<Samurai>();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            @"Server = (localdb)\MSSQLLocalDB; " +
+            "Database = EFSamurai; " +
+            "Trusted_Connection = True;");
+        }
+    }
+}
