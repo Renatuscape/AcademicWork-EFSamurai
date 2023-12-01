6596a5dbd0d9b60f085cf192237caaa933593a62
Renatuscape <silarcta@gmail.com>
2023-12-01 15:43:14 +0100

    new migrations

---
+++ EFSamurai.DataAccess/Migrations/20231201144255_SecretIdentityAddedQuoteStyleRenamed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFSamurai.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecretIdentityAddedQuoteStyleRenamed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuoteStyle",
+                table: "Quote",
+                newName: "Style");
+
+            migrationBuilder.CreateTable(
+                name: "SecretIdentity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RealName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SamuraiID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecretIdentity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecretIdentity_Samurai_SamuraiID",
+                        column: x => x.SamuraiID,
+                        principalTable: "Samurai",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecretIdentity_SamuraiID",
+                table: "SecretIdentity",
+                column: "SamuraiID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SecretIdentity");
+
+            migrationBuilder.RenameColumn(
+                name: "Style",
+                table: "Quote",
+                newName: "QuoteStyle");
+        }
+    }
+}
