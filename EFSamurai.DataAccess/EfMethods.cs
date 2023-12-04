470dc35e34fc45ed91059afa58a36e9021ded0b4
Renatuscape <silarcta@gmail.com>
2023-12-04 10:18:24 +0100

    Added static class for communicating with data base.

---
+++ EFSamurai.DataAccess/EfMethods.cs
@@ -0,0 +1,29 @@
+﻿using EFSamurai.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFSamurai.DataAccess
+{
+    public static class EfMethods
+    {
+        public static int CreateSamurai(Samurai samurai)
+        {
+            using SamuraiDbContext db = new();
+            db.Samurai.Add(samurai);
+            db.SaveChanges();
+            // Id got a new value (is no longer 0) when we did SaveChanges(), above.
+            return samurai.Id;
+        }
+
+        public static int CreateSamurai(string name)
+        {
+            // NOTE: We don't want to make two bodies of the same method if we don't
+            // have to. So in this (the more restrictive) version, we just reshape the
+            // in-parameter and call the other version.
+            return CreateSamurai(new Samurai() { Name = name });
+        }
+    }
+}
