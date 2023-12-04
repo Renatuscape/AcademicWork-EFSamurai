2d8fa44930dd32b565a979e3901c5d3fe96d1228
Renatuscape <silarcta@gmail.com>
2023-12-04 09:53:38 +0100

    Updated migrations.

---
+++ EFSamurai.DataAccess/Migrations/20231204084736_BattleAndSamuraiBattleAdded.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFSamurai.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class BattleAndSamuraiBattleAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Battle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsBrutal = table.Column<bool>(type: "bit", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Battle", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "samuraiBattles",
+                columns: table => new
+                {
+                    SamuraiId = table.Column<int>(type: "int", nullable: false),
+                    BattleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_samuraiBattles", x => new { x.SamuraiId, x.BattleId });
+                    table.ForeignKey(
+                        name: "FK_samuraiBattles_Battle_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battle",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_samuraiBattles_Samurai_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurai",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_samuraiBattles_BattleId",
+                table: "samuraiBattles",
+                column: "BattleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "samuraiBattles");
+
+            migrationBuilder.DropTable(
+                name: "Battle");
+        }
+    }
+}
---
+++ EFSamurai.DataAccess/Migrations/20231204085137_SamuraiBattleCaseChange.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFSamurai.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SamuraiBattleCaseChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_samuraiBattles_Battle_BattleId",
+                table: "samuraiBattles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_samuraiBattles_Samurai_SamuraiId",
+                table: "samuraiBattles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_samuraiBattles",
+                table: "samuraiBattles");
+
+            migrationBuilder.RenameTable(
+                name: "samuraiBattles",
+                newName: "SamuraiBattles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_samuraiBattles_BattleId",
+                table: "SamuraiBattles",
+                newName: "IX_SamuraiBattles_BattleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SamuraiBattles",
+                table: "SamuraiBattles",
+                columns: new[] { "SamuraiId", "BattleId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SamuraiBattles_Battle_BattleId",
+                table: "SamuraiBattles",
+                column: "BattleId",
+                principalTable: "Battle",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SamuraiBattles_Samurai_SamuraiId",
+                table: "SamuraiBattles",
+                column: "SamuraiId",
+                principalTable: "Samurai",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SamuraiBattles_Battle_BattleId",
+                table: "SamuraiBattles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SamuraiBattles_Samurai_SamuraiId",
+                table: "SamuraiBattles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SamuraiBattles",
+                table: "SamuraiBattles");
+
+            migrationBuilder.RenameTable(
+                name: "SamuraiBattles",
+                newName: "samuraiBattles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SamuraiBattles_BattleId",
+                table: "samuraiBattles",
+                newName: "IX_samuraiBattles_BattleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_samuraiBattles",
+                table: "samuraiBattles",
+                columns: new[] { "SamuraiId", "BattleId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_samuraiBattles_Battle_BattleId",
+                table: "samuraiBattles",
+                column: "BattleId",
+                principalTable: "Battle",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_samuraiBattles_Samurai_SamuraiId",
+                table: "samuraiBattles",
+                column: "SamuraiId",
+                principalTable: "Samurai",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
