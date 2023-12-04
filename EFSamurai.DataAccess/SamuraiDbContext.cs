e9f06787bdd6924dbe667f4458a7adc3c349f252
Renatuscape <silarcta@gmail.com>
2023-12-04 09:52:29 +0100

    Added Battle and SamuraiBattle tables. Created override for SamuraiBattle composite primary key.

--- EFSamurai.DataAccess/SamuraiDbContext.cs
+++ EFSamurai.DataAccess/SamuraiDbContext.cs
@@ -13,12 +13,19 @@ namespace EFSamurai.DataAccess
         public DbSet<Samurai> Samurai => Set<Samurai>();
         public DbSet<Quote> Quote => Set<Quote>();
         public DbSet<SecretIdentity> SecretIdentity => Set<SecretIdentity>();
+        public DbSet<Battle> Battle => Set<Battle>();
+        public DbSet<SamuraiBattle> SamuraiBattles => Set<SamuraiBattle>();
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
             @"Server = (localdb)\MSSQLLocalDB; " +
             "Database = EFSamurai; " +
             "Trusted_Connection = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
         }
     }
 }
