0e8c3b8b3b34a3414f8e932daa2049f5e26e7234
Renatuscape <silarcta@gmail.com>
2023-12-04 10:54:34 +0100

    Added specialised console output class with methods.

---
+++ EFSamurai.App/OutputConsole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFSamurai.App
+{
+    public static class OutputConsole
+    {
+        public static void DisplayTitle(string title)
+        {
+            Console.WriteLine(Tabulate("*** " + title + " ****\n"));
+        }
+        public static void DisplayText(string text)
+        {
+            Console.WriteLine(Tabulate(text));
+        }
+        public static void DisplayStringList(List<string> list)
+        {
+            foreach (string s in list)
+            {
+                DisplayText(s);
+            }
+        }
+
+        static string Tabulate(string text)
+        {
+            text = "\t" + text.Replace("\n", "\n\t");
+            return text;
+        }
+    }
+}
