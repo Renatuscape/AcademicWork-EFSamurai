64ba70b4dfea688ccf27a92cf1d45d2181bb2a27
Renatuscape <silarcta@gmail.com>
2023-12-04 14:00:58 +0100

    Updated migrations

--- EFSamurai.DataAccess/Migrations/20231204125923_BugFixBattleLogCollectionDataType.Designer.cs
+++ EFSamurai.DataAccess/Migrations/20231204125923_BugFixBattleLogCollectionDataType.Designer.cs
@@ -4,6 +4,7 @@ using EFSamurai.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFSamurai.DataAccess.Migrations
 {
     [DbContext(typeof(SamuraiDbContext))]
-    partial class SamuraiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231204125923_BugFixBattleLogCollectionDataType")]
+    partial class BugFixBattleLogCollectionDataType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EFSamurai.DataAccess/Migrations/20231204125923_BugFixBattleLogCollectionDataType.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFSamurai.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class BugFixBattleLogCollectionDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BattleLog_BattleLog_BattleLogId",
+                table: "BattleLog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BattleLog_BattleLogId",
+                table: "BattleLog");
+
+            migrationBuilder.DropColumn(
+                name: "BattleLogId",
+                table: "BattleLog");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BattleLogId",
+                table: "BattleLog",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BattleLog_BattleLogId",
+                table: "BattleLog",
+                column: "BattleLogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BattleLog_BattleLog_BattleLogId",
+                table: "BattleLog",
+                column: "BattleLogId",
+                principalTable: "BattleLog",
+                principalColumn: "Id");
+        }
+    }
+}
