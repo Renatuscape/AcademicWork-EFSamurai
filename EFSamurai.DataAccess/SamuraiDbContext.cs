69fa5364ab8b06596e20dfa2f18e52bd81e18704
Renatuscape <silarcta@gmail.com>
2023-12-04 13:34:56 +0100

    Added methods to rebuild and clear database

--- EFSamurai.DataAccess/SamuraiDbContext.cs
+++ EFSamurai.DataAccess/SamuraiDbContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace EFSamurai.DataAccess
 {
@@ -28,6 +29,48 @@ namespace EFSamurai.DataAccess
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+        }
+
+        public static void RebuildDatabase()
+        {
+            using SamuraiDbContext db = new();
+            // Deletes the entire database:
+            db.Database.EnsureDeleted();
+            // Recreates the DB tables, based on the Migrations folder data.
+            db.Database.Migrate();
+            // NOTE: EnsureCreated(), below, also recreates the DB tables. But
+            // it doesn't play well with migrations. Only use it if you
+            // don't plan to build further on DB with migrations later.
+            //db.Database.EnsureCreated();
+        }
+
+        public void ResetIdentityStartingValue(string tableName, int startingValue = 1)
+        {
+            Database.ExecuteSqlRaw("IF EXISTS(SELECT * FROM sys.identity_columns " +
+            "WHERE OBJECT_NAME(OBJECT_ID) = @tableName AND last_value IS NOT NULL) " +
+            "DBCC CHECKIDENT(@tableName, RESEED, @startingValueMinusOne);",
+            new SqlParameter("tablename", tableName),
+            new SqlParameter("startingValueMinusOne", startingValue - 1));
+        }
+
+        public static void ClearAllData()
+        {
+            using SamuraiDbContext db = new();
+
+            // 1) Note: As cascading delete is on (by default), it is enough to remove Samurais and Battles.
+            // Deleting the rows in these two tables drags ("cascades") everything else with them.
+            db.RemoveRange(db.Samurai);
+            db.RemoveRange(db.Battle);
+
+            // 2) Restart IDENTITY counting at 1 for tables with auto-incrementing PKs (6 of the 7 EfSamurai tables).
+            // (Note: Hardcoded tablenames, doublecheck singular/plural vs. your naming convention.)
+            db.ResetIdentityStartingValue("Samurai");
+            db.ResetIdentityStartingValue("SecretIdentity");
+            db.ResetIdentityStartingValue("Quote");
+            db.ResetIdentityStartingValue("Battle");
+            db.ResetIdentityStartingValue("BattleLog");
+            db.ResetIdentityStartingValue("BattleEvent");
+            db.SaveChanges();
         }
     }
 }
