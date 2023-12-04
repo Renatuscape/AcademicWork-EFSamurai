323e1d4432960cf20edc5e4705375a8c0e1d2042
Renatuscape <silarcta@gmail.com>
2023-12-04 10:17:55 +0100

    Created console app project to test database.

---
+++ EFSamurai.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using EFSamurai.DataAccess;
+
+namespace EFSamurai.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EfMethods.CreateSamurai("Tomoe Gozen");
+        }
+    }
+}
