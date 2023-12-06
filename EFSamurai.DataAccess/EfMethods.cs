022a10e9c61d271a4b8b190e2dd55b8646b0a44f
Renatuscape <silarcta@gmail.com>
2023-12-06 13:54:39 +0100

    Improved methods to create samurai with related data and refactored with regions

--- EFSamurai.DataAccess/EfMethods.cs
+++ EFSamurai.DataAccess/EfMethods.cs
@@ -24,12 +24,6 @@ namespace EFSamurai.DataAccess
             return CreateSamurai(new Samurai() { Name = name });
         }
 
-        public static List<string> ReadAllSamuraiNames()
-        {
-            using SamuraiDbContext db = new();
-            return db.Samurai.Select(s => s.Name).ToList();
-        }
-
         public static void CreateSamurais(List<Samurai> samurais)
         {
             using SamuraiDbContext db = new();
@@ -37,21 +31,84 @@ namespace EFSamurai.DataAccess
             db.SaveChanges();
         }
 
-        public static Samurai? ReadSamurai(int id)
+        public static Samurai CreateSamuraiWithRelatedData(Samurai samurai, string realName, List<Quote> quotes, List<Battle> battles)
         {
-
             using SamuraiDbContext db = new();
+            using var transaction = db.Database.BeginTransaction();
 
-            return db.Samurai.Where(s => s.Id == id)
-                .Include(s => s.SecretIdentity)
-                .Include(s => s.Quotes)
-                .Include(s => s.SamuraiBattles)!
-                .ThenInclude(sb => sb.Battle)
-                .ThenInclude(b => b!.BattleLog)
-                .ThenInclude(bl => bl!.BattleEvents)
-                .SingleOrDefault();
+            try
+            {
+                samurai.Quotes = quotes;
+
+                //List<int> idList = new() { samurai.Id };
+
+                samurai.SamuraiBattles = new List<SamuraiBattle>();
+                foreach (Battle battle in battles)
+                {
+                    samurai.SamuraiBattles.Add(new SamuraiBattle() { BattleId = battle.Id });
+                }
+
+                samurai.SecretIdentity = new() {RealName = realName };
+                db.Samurai.Add(samurai);
+                db.SaveChanges();
+                transaction.Commit();
+
+                return samurai;
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
+
+                throw;
+            }
         }
 
+        public static int CreateSamuraiWithRelatedData(Samurai samurai)
+        {
+            using SamuraiDbContext db = new();
+            using var transaction = db.Database.BeginTransaction();
+
+            try
+            {
+                db.Samurai.Add(samurai);
+
+                if (samurai.SecretIdentity == null)
+                {
+                    samurai.SecretIdentity = new SecretIdentity();
+                }
+
+                if (samurai.Quotes == null)
+                {
+                    samurai.Quotes = new List<Quote>();
+                }
+
+                if (samurai.SamuraiBattles == null)
+                {
+                    samurai.SamuraiBattles = new List<SamuraiBattle>();
+                }
+
+                db.SaveChanges();
+                transaction.Commit();
+
+                return samurai.Id;
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+        public static int CreateBattle(Battle battle)
+        {
+            using SamuraiDbContext db = new();
+            db.Battle.Add(battle);
+            db.SaveChanges();
+            return battle.Id;
+        }
+
+        #region Delete Methods
         public static void DeleteSamurai(Samurai samurai)
         {
             using SamuraiDbContext db = new();
@@ -71,32 +128,9 @@ namespace EFSamurai.DataAccess
             }
             return false;
         }
+        #endregion
 
-        public static bool UpdateSamuraiSetSecretIdentityRealName(int samuraiId, string realName)
-        {
-            using SamuraiDbContext db = new();
-            SecretIdentity? secretIdentity = db.SecretIdentity.Where(s => s.SamuraiID == samuraiId).SingleOrDefault();
-
-            if (secretIdentity is not null)
-            {
-                secretIdentity.RealName = realName;
-                db.Update(secretIdentity);
-                db.SaveChanges();
-                return true;
-            }
-            return false;
-        }
-        public static int CreateBattle(Battle battle)
-        {
-            using SamuraiDbContext db = new();
-            db.Battle.Add(battle);
-            db.SaveChanges();
-
-            return battle.Id;
-            //If the battle object in the in parameter has battle log with battle events attached
-            //those objects will also be added in their respective columns automatically
-        }
-
+        #region Update Methods
         public static void LinkBattleAndSamurais(int battleId, List<int> samuraiIds)
         {
             using SamuraiDbContext db = new();
@@ -125,7 +159,85 @@ namespace EFSamurai.DataAccess
                 db.SaveChanges();
             }
         }
+        public static bool UpdateSamuraiSetSecretIdentityRealName(int samuraiId, string realName)
+        {
+            using SamuraiDbContext db = new();
+            Samurai? samurai = db.Samurai.Where(s => s.Id == samuraiId)
+                .Include(s => s.SecretIdentity).SingleOrDefault();
 
+            if (samurai is not null)
+            {
+                if (samurai.SecretIdentity is null)
+                {
+                    samurai.SecretIdentity = new();
+                }
+
+                samurai.SecretIdentity.RealName = realName;
+                db.Update(samurai.SecretIdentity);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Read Methods
+        public static List<string> StringifyQuotesOfStyleAddSamuraiName(QuoteStyle quoteStyle)
+        {
+            List<string> stringified = new();
+            using SamuraiDbContext db = new();
+            List<Quote> quotes = db.Quote.Where(q => q.Style == quoteStyle)
+                .Include(q => q.Samurai).ToList();
+
+            foreach (Quote quote in quotes)
+            {
+                stringified.Add($"'{quote.Text}' is a {quote.Style} quote by {quote.Samurai?.Name ?? "Unknown"}");
+            }
+            return stringified;
+        }
+
+        public static string StringifySamuraiWithRelatedData(int samuraiId)
+        {
+            StringBuilder samuraiText = new();
+            using SamuraiDbContext db = new();
+            Samurai? samurai = db.Samurai
+                .Where(s => s.Id == samuraiId)
+                .Include(s => s.SecretIdentity)
+                .Include(s => s.Quotes)
+                .Include(s => s.SamuraiBattles)!
+                .ThenInclude(sb => sb.Battle)
+                .ThenInclude(b => b!.BattleLog)
+                .ThenInclude(bl => bl!.BattleEvents)
+                .SingleOrDefault();
+
+            if (samurai != null)
+            {
+                samuraiText.AppendLine(new string('=', 25));
+                samuraiText.AppendLine("Name: " + samurai.Name);
+                samuraiText.AppendLine("Secret Identity: " + samurai.SecretIdentity?.RealName);
+                samuraiText.AppendLine("Hairstyle: " + samurai.HairStyle.ToString());
+                if (samurai.Quotes != null)
+                {
+                    samuraiText.AppendLine(new string('-', 25));
+                    samuraiText.AppendLine("Quotes:");
+                    foreach (Quote quote in samurai.Quotes)
+                    {
+                        samuraiText.AppendLine("\t" + quote.Text);
+                    }
+                }
+                if (samurai.SamuraiBattles != null)
+                {
+                    samuraiText.AppendLine(new string('-', 25));
+                    samuraiText.AppendLine("Battles: ");
+                    foreach (SamuraiBattle link in samurai.SamuraiBattles)
+                    {
+                        samuraiText.AppendLine(link.Battle?.Name);
+                        samuraiText.AppendLine(link.Battle?.BattleLog?.ToString());
+                    }
+                }
+            }
+            return samuraiText.ToString();
+        }
         public static int CountBattlesForSamurai(int samuraiId, bool? isBrutal = null)
         {
             using SamuraiDbContext db = new();
@@ -153,48 +265,18 @@ namespace EFSamurai.DataAccess
             using SamuraiDbContext db = new();
             return db.Quote.Where(q => q.Style == quoteStyle).ToList();
         }
-
-        public static List<string> StringifyQuotesOfStyleAddSamuraiName(QuoteStyle quoteStyle)
-        {
-            List<string> stringified = new();
-            using SamuraiDbContext db = new();
-            List<Quote> quotes =  db.Quote.Where(q => q.Style == quoteStyle)
-                .Include(q => q.Samurai).ToList();
-
-            foreach (Quote quote in quotes)
-            {
-                stringified.Add($"'{quote.Text}' is a {quote.Style} quote by {quote.Samurai?.Name ?? "Unknown"}");
-            }
-            return stringified;
-        }
-        public static Samurai CreateSamuraiWithRelatedData(Samurai samurai, string realName, List<Quote> quotes, List<Battle> battles)
+        public static List<string> ReadAllSamuraiNames()
         {
             using SamuraiDbContext db = new();
-            samurai.Quotes = quotes;
-            db.Samurai.Add(samurai);
-            db.SaveChanges();
-
-            List<int> idList = new() { samurai.Id };
-
-            foreach (Battle battle in battles)
-            {
-                LinkBattleAndSamurais(battle.Id, idList);
-            }
-            SecretIdentity identity = new();
-            identity.RealName = realName;
-            identity.SamuraiID = samurai.Id;
-            db.SecretIdentity.Add(identity);
-            db.SaveChanges();
-
-            return samurai;
+            return db.Samurai.Select(s => s.Name).ToList();
         }
 
-        public static string PrintSamuraiWithRelatedData(int samuraiId)
+        public static Samurai? ReadSamurai(int id)
         {
-            StringBuilder samuraiText = new();
+
             using SamuraiDbContext db = new();
-            Samurai? samurai = db.Samurai
-                .Where(s => s.Id == samuraiId)
+
+            return db.Samurai.Where(s => s.Id == id)
                 .Include(s => s.SecretIdentity)
                 .Include(s => s.Quotes)
                 .Include(s => s.SamuraiBattles)!
@@ -202,72 +284,7 @@ namespace EFSamurai.DataAccess
                 .ThenInclude(b => b!.BattleLog)
                 .ThenInclude(bl => bl!.BattleEvents)
                 .SingleOrDefault();
-
-            if (samurai != null)
-            {
-                samuraiText.AppendLine(new string('=', 25));
-                samuraiText.AppendLine("Name: " + samurai.Name);
-                samuraiText.AppendLine("Secret Identity: " + samurai.SecretIdentity?.RealName);
-                samuraiText.AppendLine("Hairstyle: " + samurai.HairStyle.ToString());
-                if (samurai.Quotes != null)
-                {
-                    samuraiText.AppendLine(new string ('-',25));
-                    samuraiText.AppendLine("Quotes:");
-                    foreach (Quote quote in samurai.Quotes)
-                    {
-                        samuraiText.AppendLine("\t" + quote.Text);
-                    }
-                }
-                if (samurai.SamuraiBattles != null)
-                {
-                    samuraiText.AppendLine(new string('-', 25));
-                    samuraiText.AppendLine("Battles: ");
-                    foreach (SamuraiBattle link in samurai.SamuraiBattles)
-                    {
-                        samuraiText.AppendLine(link.Battle?.Name);
-                        samuraiText.AppendLine(link.Battle?.BattleLog?.ToString());
-                    }
-                }
-            }
-            return samuraiText.ToString();
         }
-        public static int CreateSamuraiWithRelatedData(Samurai samurai)
-        {
-            using SamuraiDbContext db = new();
-            using var transaction = db.Database.BeginTransaction();
-
-            try
-            {
-                db.Samurai.Add(samurai);
-
-                if (samurai.SecretIdentity != null)
-                {
-                    db.SecretIdentity.Add(samurai.SecretIdentity);
-                }
-
-                if (samurai.Quotes != null)
-                {
-                    db.Quote.AddRange(samurai.Quotes);
-                }
-
-                if (samurai.SamuraiBattles != null)
-                {
-                    foreach (SamuraiBattle link in samurai.SamuraiBattles)
-                    {
-                        LinkBattleAndSamurais(link.BattleId, new List<int> { link.SamuraiId });
-                    }
-                }
-
-                db.SaveChanges();
-                transaction.Commit();
-
-                return samurai.Id;
-            }
-            catch (Exception)
-            {
-                transaction.Rollback();
-                throw;
-            }
-        }
+        #endregion
     }
 }
