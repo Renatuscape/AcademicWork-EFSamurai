ab863547a25766b7f749cde698406d040c9cf419
Renatuscape <silarcta@gmail.com>
2023-12-06 11:50:22 +0100

    Added ToString method

--- EFSamurai.Domain/Entities/BattleLog.cs
+++ EFSamurai.Domain/Entities/BattleLog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Azure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,21 @@ namespace EFSamurai.Domain.Entities
         public int BattleId { get; set; }
         public Battle? Battle { get; set; }
         public ICollection<BattleEvent>? BattleEvents { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder text = new();
+            text.AppendLine(Name);
+            if (BattleEvents != null)
+            {
+                text.AppendLine("Events:");
+                foreach (BattleEvent batEvent in BattleEvents)
+                {
+                    text.AppendLine($"\t Order: {batEvent.Order} Summary: {batEvent.Summary}");
+                }
+            }
+
+            return text.ToString();
+        }
     }
 }
