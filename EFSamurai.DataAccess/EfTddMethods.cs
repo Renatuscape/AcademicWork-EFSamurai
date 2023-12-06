e5eda75be64e1d393ed2cb76d12a82c23c79800d
Renatuscape <silarcta@gmail.com>
2023-12-06 10:02:06 +0100

    Solved TDD exercise.

---
+++ EFSamurai.DataAccess/EfTddMethods.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EFSamurai.Domain;
+using EFSamurai.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFSamurai.DataAccess
+{
+	public class EfTddMethods
+	{
+		#region Helper methods
+
+		public static void RebuildDatabase()
+		{
+			using SamuraiDbContext db = new();
+
+			db.Database.EnsureDeleted();
+			db.Database.Migrate();
+			//db.Database.EnsureCreated();
+		}
+
+
+		public static void ClearAllData()
+		{
+			using SamuraiDbContext db = new();
+
+			db.RemoveRange(db.Samurai);
+			db.RemoveRange(db.Battle);
+
+			db.ResetIdentityStartingValue("Samurai");
+			db.ResetIdentityStartingValue("SecretIdentity");
+			db.ResetIdentityStartingValue("Quote");
+			db.ResetIdentityStartingValue("Battle");
+			db.ResetIdentityStartingValue("BattleLog");
+			db.ResetIdentityStartingValue("BattleEvent");
+
+			db.SaveChanges();
+		}
+
+		#endregion Helper methods
+
+		public static int CreateSamurai(string name, HairStyle? hairStyle)
+		{
+			Samurai samurai = new() { Name = name, HairStyle = hairStyle };
+			return CreateSamurai(samurai);
+		}
+
+		public static int CreateSamurai(Samurai samurai)
+		{
+            using SamuraiDbContext db = new();
+            db.Add(samurai);
+            db.SaveChanges();
+            return samurai.Id;
+        }
+		public static List<string> ReadAlphabeticallyAllSamuraiNamesWithSpecificHairstyle(HairStyle hairStyle)
+		{
+			using SamuraiDbContext db = new();
+			return db.Samurai.Where(s => s.HairStyle == hairStyle).OrderBy(s => s.Name).Select(s => s.Name).ToList();
+		}
+
+		public static List<Quote> ReadAllQuotesWithSpecificQuoteStyle(QuoteStyle quoteStyle)
+		{
+            using SamuraiDbContext db = new();
+            return db.Quote
+				.Include(q => q.Samurai)
+				.Where(q => q.Style == quoteStyle).ToList();
+        }
+
+		public static int CreateBattle(string battleName, bool isBrutal, string description, DateTime startDate, DateTime endDate)
+		{
+			Battle battle = new Battle() { Name = battleName, IsBrutal = isBrutal, Description = description, StartDate = startDate, EndDate = endDate};
+            using SamuraiDbContext db = new();
+			db.Battle.Add(battle);
+			db.SaveChanges();
+			return battle.Id;
+		}
+        public static Battle? ReadOneBattle(int battleId)
+		{
+            using SamuraiDbContext db = new();
+            return db.Battle.Where(b => b.Id == battleId).SingleOrDefault();
+		}
+
+        public static void CreateOrUpdateSecretIdentitySetRealName(int samuraiId, string name)
+		{
+            using SamuraiDbContext db = new();
+			Samurai? samurai = db.Samurai
+				.Include(s => s.SecretIdentity)
+				.Where(s => s.Id == samuraiId).SingleOrDefault();
+
+			if (samurai is not null)
+			{
+                samurai.SecretIdentity ??= new SecretIdentity();
+                samurai.SecretIdentity.RealName = name;
+
+				db.SaveChanges();
+            }
+        }
+
+        public static SecretIdentity? ReadSecretIdentityOfSpecificSamurai(int samuraiId)
+		{
+            using SamuraiDbContext db = new();
+            Samurai? samurai = db.Samurai
+				.Include(s => s.SecretIdentity)
+				.Where(s => s.Id == samuraiId).SingleOrDefault();
+			return samurai?.SecretIdentity;
+        }
+    }
+}
---
+++ EFSamurai.NUnitTest/TddUnitTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using EFSamurai.DataAccess;
+using EFSamurai.Domain;
+using EFSamurai.Domain.Entities;
+using NUnit.Framework;
+
+namespace EFSamurai.NUnitTest
+{
+    public class TddUnitTests
+	{
+		[OneTimeSetUp]
+		public void OneTimeSetup()
+		{
+			EfTddMethods.RebuildDatabase();
+		}
+
+
+		[SetUp]
+		public void Setup()
+		{
+			EfTddMethods.ClearAllData();
+		}
+
+
+		[Test]
+		public void Test1_AddThreeSamuraisListNamesOfThoseWithWesternHairstyles()
+		{
+			EfTddMethods.CreateSamurai("Williams Adams", HairStyle.Western);
+			EfTddMethods.CreateSamurai("Tokugawa Leyasu", HairStyle.Oicho);
+			EfTddMethods.CreateSamurai("Jan Joosten", HairStyle.Western);
+			List<string> result = EfTddMethods.ReadAlphabeticallyAllSamuraiNamesWithSpecificHairstyle(HairStyle.Western);
+			CollectionAssert.AreEqual(new List<string>(){ "Jan Joosten", "Williams Adams" }, result);
+		}
+
+
+		[Test]
+		public void Test2_AddThreeSamuraisWithQuotesReadCheesyQuotes()
+		{
+			EfTddMethods.CreateSamurai(new Samurai(){ Name = "Himura Kenshin", Quotes = new List<Quote>(){ new(){ Text = "You can always die. It's living that takes real courage.", Style = QuoteStyle.Awesome }}});
+			EfTddMethods.CreateSamurai(new Samurai(){ Name = "Watsuki Nobuhiro", Quotes = new List<Quote>(){ new(){ Text = "New eras don't come about because of swords, they're created by the people who wield them.", Style = QuoteStyle.Cheesy }}});
+			EfTddMethods.CreateSamurai(new Samurai(){ Name = "Laini Taylor", Quotes = new List<Quote>(){ new(){ Text = "Be a Samurai. Because you just never know what's behind the freaking sky.", Style = QuoteStyle.Lame }}});
+			List<Quote> result = EfTddMethods.ReadAllQuotesWithSpecificQuoteStyle(QuoteStyle.Cheesy);
+			Assert.That(result[0].Text, Is.EqualTo("New eras don't come about because of swords, they're created by the people who wield them."));
+			Assert.That(result[0].Samurai?.Name, Is.EqualTo("Watsuki Nobuhiro"));
+		}
+
+
+		[Test]
+		public void Test3_AddOneBattle()
+		{
+			int battleId = EfTddMethods.CreateBattle("WW2", true, "Sauerkraut", new DateTime(1939, 10, 10), new DateTime(1945, 10, 10));
+			Battle? actualBattle = EfTddMethods.ReadOneBattle(battleId);
+			Assert.That(actualBattle?.Name, Is.EqualTo("WW2"));
+			Assert.That(actualBattle?.IsBrutal, Is.EqualTo(true));
+			Assert.That(actualBattle?.Description, Is.EqualTo("Sauerkraut"));
+			Assert.That(actualBattle?.StartDate, Is.EqualTo(new DateTime(1939, 10, 10)));
+			Assert.That(actualBattle?.EndDate, Is.EqualTo(new DateTime(1945, 10, 10)));
+		}
+
+
+		[Test]
+		public void Test4_AddSecretIdentity()
+		{
+			int samuraiId = EfTddMethods.CreateSamurai("Papa Smurf", HairStyle.Western);
+
+			EfTddMethods.CreateOrUpdateSecretIdentitySetRealName(samuraiId, "Rollo Tomasi");
+			SecretIdentity? result = EfTddMethods.ReadSecretIdentityOfSpecificSamurai(samuraiId);
+			Assert.That(result?.Samurai?.Name, Is.EqualTo("Papa Smurf"));
+			Assert.That(result?.RealName, Is.EqualTo("Rollo Tomasi"));
+
+			EfTddMethods.CreateOrUpdateSecretIdentitySetRealName(samuraiId, "Tomas");
+			result = EfTddMethods.ReadSecretIdentityOfSpecificSamurai(samuraiId);
+			Assert.That(result?.RealName, Is.EqualTo("Tomas"));
+		}
+	}
+}
